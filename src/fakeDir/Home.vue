b67a966101f59a4c43ee750f77b500c20e2ff5aa
曾鹏程 <171250513@smail.nju.edu.cn>
2022-03-13 19:31:18 +0800

    修复主页加载慢问题

--- src/fakeDir/Home.vue
+++ src/fakeDir/Home.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="wrapper">
-    <v-head></v-head>
-    <v-sidebar></v-sidebar>
-    <div class="content-box" :class="{ 'content-collapse': collapse }">
-      <v-tags></v-tags>
-      <div class="content">
-        <transition name="move" mode="out-in">
-          <keep-alive :include="tagsList">
-            <router-view></router-view>
-          </keep-alive>
-        </transition>
-        <el-backtop target=".content"></el-backtop>
-      </div>
-    </div>
+<!--    <v-head></v-head>-->
+<!--    <v-sidebar></v-sidebar>-->
+<!--    <div class="content-box" :class="{ 'content-collapse': collapse }">-->
+<!--      <v-tags></v-tags>-->
+<!--      <div class="content">-->
+<!--        <transition name="move" mode="out-in">-->
+<!--          <keep-alive :include="tagsList">-->
+<!--            <router-view></router-view>-->
+<!--          </keep-alive>-->
+<!--        </transition>-->
+<!--        <el-backtop target=".content"></el-backtop>-->
+<!--      </div>-->
+<!--    </div>-->
   </div>
 </template>
 
